0745a3a51bab6115911aff7ea4bd53ffe1c4d318
boris <boris@iis.sinica.edu.tw>
2012-11-18 22:29:36 +0800

    filling y_petri.rb

--- lib/y_petri.rb
+++ lib/y_petri.rb
@@ -42,4 +42,51 @@ module YPetri
            :set_step, :set_sampling, :set_time,
            :run!, :plot_recording, :new_timed_simulation,
            to: :$YPetriManipulatorInstance
+
+  autoreq :place
+  autoreq :transition
+  autoreq :net
+
+  autoreq :simulation
+  autoreq :timed_simulation
+  # autoreq :hybrid_timed_simulation # not yet, LATER
+
+  autoreq :workspace
+  autoreq :manipulator
+  
+  # Expects either a Place instance, or a name of an existing Place
+  # instance. Place instance is returned unchanged, while if name was given,
+  # correspondingly named instance is returned.
+  def self.Place arg
+    case arg
+    when Place then return arg
+    when String, Symbol then
+      ( Place.instances.with_values( &:demodulize ).rassoc arg.to_s or
+          raise AE, "Unknown place name: #{arg}" )[0]
+    else raise AE, "Unexpected argument class: #{arg.class}" end
+  end
+
+  # Expects either a Transition instance, or a name of an existing
+  # Transition instance. Transition instance is returned unchanged, while if
+  # name was given, correspondingly named instance is returned.
+  def self.Transition arg
+    case arg
+    when Transition then return arg
+    when String, Symbol then
+      ( Transition.instances.with_values( &:demodulize ).rassoc arg.to_s or
+          raise AE, "Unknown place name: #{arg}" )[0]
+    else raise AE, "Unexpected argument class: #{arg.class}" end
+  end
+
+  # Expects either a net instance, or a name of an existing net instance.
+  # Net instance is returned unchanged, while if name was given,
+  # correspondingly named instance is returned.
+  def self.Net arg
+    case arg
+    when Net then return arg
+    when String, Symbol then
+      ( Net.instances.with_values( &:demodulize ).rassoc arg.to_s or
+          raise AE, "Unknown place name: #{arg}" )[0]
+    else raise AE, "Unexpected argument class: #{arg.class}" end
+  end
 end
