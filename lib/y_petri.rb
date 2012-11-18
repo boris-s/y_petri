6fd13a3d113fe58b9738f31fd3e32834a1ebf677
boris <boris@iis.sinica.edu.tw>
2012-11-18 22:28:56 +0800

    filling y_petri.rb

--- lib/y_petri.rb
+++ lib/y_petri.rb
@@ -24,5 +24,22 @@ require 'const_magic_ersatz'
 require 'y_petri/version'
 
 module YPetri
+  DEFAULT_SIMULATION_SETTINGS =
+    { step_size: 1, sampling_period: 10, target_time: 60 }
 
+  def self.included( receiver )
+    $YPetriManipulatorInstance = ::YPetri::Manipulator.new
+  end
+
+  delegate :workspace,
+           :places, :transitions, :nets, :simulations,
+           :ccc, :imcc, :sscc,
+           :pp, :tt, :nn,
+           :print_recording, :plot_recording,
+           :net, :simulation, :ssc, :cc, :imc,
+           :place, :transition, :p, :t,
+           :clamp, :initial_marking, :im,
+           :set_step, :set_sampling, :set_time,
+           :run!, :plot_recording, :new_timed_simulation,
+           to: :$YPetriManipulatorInstance
 end
