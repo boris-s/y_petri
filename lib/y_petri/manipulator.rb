c715124dfcd054e5745b641d98ef909bd6e1b335
boris <boris@iis.sinica.edu.tw>
2012-11-18 22:26:20 +0800

    + workspace & manipulator

---
+++ lib/y_petri/manipulator.rb
@@ -0,0 +1,7 @@
+#encoding: utf-8
+
+module YPetri
+  class Manipulator
+    
+  end # class Manipulator
+end # module YPetri
---
+++ lib/y_petri/workspace.rb
@@ -0,0 +1,7 @@
+#encoding: utf-8
+
+module YPetri
+  class Workspace
+    
+  end # class Workspace
+end # module YPetri
